02de0b709d5f2bdd817b5649b26126e1df41b13e
Daniel Kaspo <304269+NessDan@users.noreply.github.com>
2022-02-28 19:36:52 -0500

    Removed your password

--- skillshare.py
+++ skillshare.py
@@ -6,7 +6,7 @@ import youtube_dl
 
 ydl_opts = {
     "username": "chadboyce@gmail.com",
-    "password": "crakr0x1128"
+    "password": "REPLACEME"
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.skillshare.com/classes/Calligraphy-Essentials-From-First-Script-to-Final-Flourish/1829141067/00'])
